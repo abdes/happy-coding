33b4462f4a2b4520b49ed6707c269144b5d8daea
Abdessattar Sassi <abde.sassi@gmail.com>
2020-06-22 14:25:35 +0400

    chore: resolve pre-existing lint errors

--- apps/example/src/app/app.component.spec.ts
+++ apps/example/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'example'`, () => {
+  it('should have as title "example"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('example');
--- apps/example/src/app/app.component.ts
+++ apps/example/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'happy-coding-root',
+  selector: 'exmp-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
