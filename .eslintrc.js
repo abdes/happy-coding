cdeb3f11822de0d30a6171de3a809e57e8e57fd7
Abdessattar Sassi <abde.sassi@gmail.com>
2020-06-22 13:36:40 +0400

    dev(eslint): add root eslintrc.js
    We use .js instead of .json config in order to be able to cascade eslint
    congiguration from apps/libs down to master workspace config in a
    similar fashion to angular with tslint.

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+/**
+ * We are using the .JS version of an ESLint config file here so that we can
+ * add lots of comments to better explain and document the setup.
+ */
+module.exports = {
+  /**
+   * See packages/eslint-plugin/src/configs/README.md
+   * for what this recommended config contains.
+   */
+  ignorePatterns: ['**/*.js'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@angular-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // ORIGINAL tslint.json -> "directive-selector": [true, "attribute", "app", "camelCase"],
+    '@angular-eslint/directive-selector': [
+      'error',
+      { type: 'attribute', prefix: 'hc', style: 'camelCase' },
+    ],
+
+    // ORIGINAL tslint.json -> "component-selector": [true, "element", "app", "kebab-case"],
+    '@angular-eslint/component-selector': [
+      'error',
+      { type: 'element', prefix: 'hc', style: 'kebab-case' },
+    ],
+  },
+  overrides: [
+    /**
+     * This extra piece of configuration is only necessary if you make use of inline
+     * templates within Component metadata, e.g.:
+     *
+     * @Component({
+     *  template: `<h1>Hello, World!</h1>`
+     * })
+     * ...
+     *
+     * It is not necessary if you only use .html files for templates.
+     */
+    {
+      files: ['*.component.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      plugins: ['@angular-eslint/template'],
+      processor: '@angular-eslint/template/extract-inline-html',
+    },
+  ],
+};
