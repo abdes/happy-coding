35f66f6f18449c4c38abd9c5923005c30185fbfe
Abdessattar Sassi <abde.sassi@gmail.com>
2020-06-24 23:54:02 +0400

    chore(ngx-highlight): cleanup file header (copyright notice)

--- libs/ngx-highlight/src/lib/highlight.directive.ts
+++ libs/ngx-highlight/src/lib/highlight.directive.ts
@@ -1,5 +1,6 @@
-// Copyright (c) 2019-2020 The Authors.
-// This file is part of @npcz/ngx-highlight.
+// Copyright (c) 2019-2020 The Authors
+// This file is part of @npcz/ngx-highlight
+// https://github.com/abdes/happy-coding
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
