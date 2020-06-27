d9042d8061494f0e0287f7453f4943cbb136077f
Abdessattar Sassi <abde.sassi@gmail.com>
2020-06-27 16:46:14 +0400

    dev(workspace): upgrade jest and related dev deps

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
+    '^.+\\.(ts|html)$': 'ts-jest',
   },
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
