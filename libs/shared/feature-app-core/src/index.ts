4b163d334fa22c2b0020d6a4275f7c92c2d5e471
Abdessattar Sassi <abde.sassi@gmail.com>
2020-06-30 20:39:56 +0400

    feat(feature-app-core): add new lib for app-core feature

---
+++ libs/shared/feature-app-core/src/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) 2019-2020 The Authors
+// This file is the result of Happy Coding :-)
+// https://github.com/abdes/happy-coding
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+export * from './lib/public-api';
