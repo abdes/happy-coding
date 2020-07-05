beb530bd4aca66d0b14b484af1de21dc6b68596d
Abdessattar Sassi <abde.sassi@gmail.com>
2020-07-05 14:30:21 +0400

    test(ui-header): fix unit test

--- libs/shared/ui-header/src/lib/header/header.component.spec.ts
+++ libs/shared/ui-header/src/lib/header/header.component.spec.ts
@@ -7,13 +7,44 @@
 import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
 
 import { HeaderComponent } from './header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { SearchBarModule } from '@npcz/ngx-search-bar';
+import { HeaderLinksComponent } from '../header-links/header-links.component';
+import { HeaderLogoComponent } from '../header-logo/header-logo.component';
+import { HeaderRegionComponent } from '../header-region/header-region.component';
+import { HeaderUserComponent } from '../header-user/header-user.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('HeaderComponent', () => {
   let spectator: Spectator<HeaderComponent>;
   const createComponent = createComponentFactory({
     component: HeaderComponent,
-    imports: [MatToolbarModule],
+    imports: [
+      CommonModule,
+      HttpClientModule,
+      FlexLayoutModule,
+      MatButtonModule,
+      MatFormFieldModule,
+      MatIconModule,
+      MatMenuModule,
+      MatRippleModule,
+      MatSelectModule,
+      SearchBarModule,
+    ],
+    declarations: [
+      HeaderComponent,
+      HeaderLinksComponent,
+      HeaderLogoComponent,
+      HeaderRegionComponent,
+      HeaderUserComponent,
+    ],
   });
 
   it('should create', () => {
